b718c124687d5be93c1fa6785fd485835d35dd70
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2024-04-08 02:56:57 -0700

    modellar yaratildi

---
+++ roomsApp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from mainApp.models import CoreModel
+
+
+class Xona(CoreModel):
+    raqam = models.CharField(max_length=10)
+    qavat = models.PositiveSmallIntegerField()
+    joylar_soni = models.PositiveSmallIntegerField()
+    band = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Xona"
+        verbose_name_plural = "Xonalar"
+
+    def __str__(self):
+        return self.raqam
+
+
+class Joy(CoreModel):
+    raqam = models.CharField(max_length=3)
+    xona = models.ForeignKey(Xona, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Joy"
+        verbose_name_plural = "Joylar"
+
+    def __str__(self):
+        return f"{self.xona.raqam}: {self.raqam}"
+
+
+class Jihoz(CoreModel):
+    nom = models.CharField(max_length=255)
+    soni = models.PositiveSmallIntegerField()
+    xona = models.ForeignKey(Xona, on_delete=models.CASCADE)
+    izoh = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.nom}: {self.soni}"
+
---
+++ studentsApp/models.py
@@ -0,0 +1,51 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from mainApp.models import *
+from roomsApp.models import Joy
+from userApp.models import Profil
+
+
+class Guruh(CoreModel):
+    nom = models.CharField(max_length=255)
+    kurs = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    fakultet = models.ForeignKey(Fakultet, on_delete=models.SET_NULL, null=True)
+    tutor = models.ForeignKey(Profil, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Guruh"
+        verbose_name_plural = "Guruhlar"
+
+    def __str__(self):
+        return self.nom
+
+
+class Student(CoreModel):
+    JINS = (
+        ('erkak', 'erkak'),
+        ('ayol', 'ayol'),
+    )
+
+    TALIM_TURI = (
+        ('kunduzgi', 'kunduzgi'),
+        ('sirtqi', 'sirtqi'),
+    )
+
+    ism = models.CharField(max_length=255)
+    familiya = models.CharField(max_length=255)
+    otasi = models.CharField(max_length=255, blank=True, null=True)
+    jins = models.CharField(max_length=10, choices=JINS)
+    telefon_shaxsiy = models.CharField(max_length=13)
+    telefon_qoshimcha = models.CharField(max_length=13, blank=True, null=True)
+    guruh = models.CharField(max_length=10)
+    tuman = models.ForeignKey(Tuman, on_delete=models.SET_NULL, null=True)
+    manzil = models.CharField(max_length=255)
+    talim_turi = models.CharField(max_length=255, choices=TALIM_TURI)
+    joy = models.ForeignKey(Joy, on_delete=models.SET_NULL, null=True)
+    balans = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = "Talaba"
+        verbose_name_plural = "Talabalar"
+
+    def __str__(self):
+        return f"{self.ism} {self.familiya} {self.otasi}"
